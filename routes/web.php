e6036715256bbc52ecf90842526da749974927b3
longer <fl140125@gmail.com>
2019-08-19 15:16:27 +0800

    'add'

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use \Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/socket.io', function () {
+Route::any('/socket.io', function () {
     echo 'socket.io';
 });
