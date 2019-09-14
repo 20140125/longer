c450f24343a714a003736a1070842b67f4bd4861
longer <loveqin0125@foxmail.com>
2019-09-14 21:00:48 +0800

    update

--- public/webPush/config/db.php
+++ public/webPush/config/db.php
@@ -1,6 +1,9 @@
 <?php
 define('Host','127.0.0.1');
-define('Port','3306');
-define('UserName','root');
-define('Password','');
+define('Port','3309');
+define('UserName','longer');
+define('Password','F3px6jdcsXLbnbsH');
 define('DbName','longer');
+define('RedisKey','uidConnectionMap');
+define('BgImg','https://www.fanglonger.com/bg.png');
+define('KFImg','https://www.fanglonger.com/kf.jpg');
