2c029995bf0fd0c8de98f30aee1c723b83206827
longer <fl140125@gmail.com>
2019-10-16 11:19:14 +0800

    update

--- app/Mail/Register.php
+++ app/Mail/Register.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class Notice extends Mailable
+class Register extends Mailable
 {
     use Queueable, SerializesModels;
     /**
