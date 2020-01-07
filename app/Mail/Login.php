2e605a499b6df98502913954f5f0049689824eb4
longer <fl140125@gmail.com>
2020-01-07 14:14:04 +0800

    有效验证码登录

---
+++ app/Mail/Login.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Login extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $request;
+
+    /**
+     * TODO:Create a new message instance
+     * Oauth constructor.
+     * @param $request
+     */
+    public function __construct($request)
+    {
+        $this->request = $request;
+    }
+    /**
+     * @var string $subject
+     */
+    public $subject = '邮箱验证码';
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('email.login')->with([
+            'code'=>$this->request['verify_code'],
+            'url'=>config('app.url')."#/login"
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,8 @@ Route::middleware('throttle:60,1')->namespace('Api')->prefix('v1')->group(functi
     Route::match(['get','post'],'menu','v1\MenuController@getMenu')->name('menu');
     Route::match(['get','post'],'total','v1\MenuController@getCountData')->name('total');
     Route::match(['get','post'],'file/download','v1\LoginController@download')->name('downloadFile');
+    Route::match(['get','post'],'sendEmail','v1\LoginController@email')->name('sendEmail');
+    Route::match(['get','post'],'checkCode','v1\LoginController@code')->name('checkCode');
     /********************************************共有权限********************************************************/
 
     /********************************************私有权限********************************************************/
