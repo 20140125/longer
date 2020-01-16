63e461ccf1ca11fdc879171a6df82599cf692df3
longer <fl140125@gmail.com>
2020-01-16 09:46:55 +0800

    update

--- app/Http/Controllers/Api/v1/PushController.php
+++ app/Http/Controllers/Api/v1/PushController.php
@@ -38,7 +38,7 @@ class PushController extends BaseController
     public function index()
     {
         $this->validatePost(['page'=>'required|integer|gt:0','limit'=>'required|integer|gt:0']);
-        $result = $this->pushModel->getResultLists($this->post['page'],$this->post['limit'],$this->users,$this->post['state']??'',$this->post['status']??'');
+        $result = $this->pushModel->getResultLists($this->post['page'],$this->post['limit'],$this->users,$this->post['state']??0,$this->post['status']??0);
         foreach ($result['data'] as &$item) {
             $item->created_at = date('Y-m-d H:i:s',$item->created_at);
         }
