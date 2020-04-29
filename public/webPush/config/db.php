5829dc5b79bfcd66d0cd5d8ef00f2ab6670f6649
longer <loveqin0125@foxmail.com>
2020-04-29 17:35:13 +0800

    update

--- public/webPush/config/db.php
+++ public/webPush/config/db.php
@@ -2,7 +2,7 @@
 define('Host','127.0.0.1');
 define('Port','3306');
 define('UserName','root');
-define('Password','root');
+define('Password','');
 define('DbName','longer');
 define('RedisKey','uidConnectionMap');
 define('chatKey','uidChatMap'); //在线用户数
