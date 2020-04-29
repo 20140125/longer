b7187e183892c2a57822bd5e8909b50e6b405a5c
longer <fl140125@gmail.com>
2020-04-29 17:16:43 +0800

    Update db.php

--- public/webPush/config/db.php
+++ public/webPush/config/db.php
@@ -1,12 +1,8 @@
 <?php
 define('Host','127.0.0.1');
-define('Port','3309');
-define('UserName','longer');
-define('Password','F3px6jdcsXLbnbsH');
+define('Port','3306');
+define('UserName','root');
+define('Password','root');
 define('DbName','longer');
 define('RedisKey','uidConnectionMap');
-<<<<<<< HEAD
 define('chatKey','uidChatMap'); //在线用户数
-=======
-define('chatKey','uidChatMap'); //在线用户数
->>>>>>> 52bea694caf773012612db6a7624ef0c0380872c
