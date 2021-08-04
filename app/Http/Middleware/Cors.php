d8e6c23ffd9095c41c1bfef41e2ce0a12d7cd4fd
longer <fl140125@gmail.com>
2021-08-04 18:19:49 +0800

    系统优化

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * TODO:解决跨域
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+        $response->headers->set('Access-Control-Expose-Headers', 'Authorization, authenticated');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        return $response;
+    }
+}
