0a319353ac80b8787855d37644b51592f3ec8b0a
longer <fl140125@gmail.com>
2021-08-05 11:21:44 +0800

    laravel8.0/vue3单页应用系统更新

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,70 @@
-<!DOCTYPE html><html lang=""><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" href="/favicon.ico"><title>vue-app</title><script>var _hmt = _hmt || [];
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="icon" href="/favicon.ico" />
+    <title>vue-app</title>
+    <script>var _hmt = _hmt || [];
         (function() {
             var hm = document.createElement("script");
             hm.src = "https://hm.baidu.com/hm.js?f89b6a9c4850b466c82a5e1a8e73e76b";
             var s = document.getElementsByTagName("script")[0];
             s.parentNode.insertBefore(hm, s);
-        })();</script><link href="/css/chunk-11cc9a46.1127797b.css" rel="prefetch"><link href="/css/chunk-29cfb7bb.fb2a93c7.css" rel="prefetch"><link href="/css/chunk-2bc080a1.4af0aaa8.css" rel="prefetch"><link href="/css/chunk-2d4fc60c.1127797b.css" rel="prefetch"><link href="/css/chunk-30f50f1a.1127797b.css" rel="prefetch"><link href="/css/chunk-34761d0b.7a316438.css" rel="prefetch"><link href="/css/chunk-34e5ce82.dbc488f1.css" rel="prefetch"><link href="/css/chunk-3b63f688.b6549005.css" rel="prefetch"><link href="/css/chunk-48313e7b.f0504677.css" rel="prefetch"><link href="/css/chunk-5bcc2985.1127797b.css" rel="prefetch"><link href="/css/chunk-62fc455c.1127797b.css" rel="prefetch"><link href="/css/chunk-631e9f83.dbc488f1.css" rel="prefetch"><link href="/css/chunk-6cc4fa90.1127797b.css" rel="prefetch"><link href="/css/chunk-6d4bfa19.630c1498.css" rel="prefetch"><link href="/css/chunk-741560ff.0c65cad8.css" rel="prefetch"><link href="/css/chunk-9ceacaa2.c76548f8.css" rel="prefetch"><link href="/css/chunk-c3b00904.bb5df0d1.css" rel="prefetch"><link href="/js/chunk-08c34318.12fe6bf9.js" rel="prefetch"><link href="/js/chunk-11cc9a46.6598d212.js" rel="prefetch"><link href="/js/chunk-29cfb7bb.5b798f11.js" rel="prefetch"><link href="/js/chunk-2bc080a1.f9084052.js" rel="prefetch"><link href="/js/chunk-2d4fc60c.6b033114.js" rel="prefetch"><link href="/js/chunk-30f50f1a.1099e916.js" rel="prefetch"><link href="/js/chunk-34761d0b.b9256273.js" rel="prefetch"><link href="/js/chunk-34e5ce82.7df214c4.js" rel="prefetch"><link href="/js/chunk-3b63f688.f77fdc31.js" rel="prefetch"><link href="/js/chunk-48313e7b.2f6b569f.js" rel="prefetch"><link href="/js/chunk-502d2fdd.59c254bd.js" rel="prefetch"><link href="/js/chunk-5bcc2985.a43d64f1.js" rel="prefetch"><link href="/js/chunk-62fc455c.9dcb5c0a.js" rel="prefetch"><link href="/js/chunk-631e9f83.6f677539.js" rel="prefetch"><link href="/js/chunk-6cc4fa90.925916ca.js" rel="prefetch"><link href="/js/chunk-6d4bfa19.db75a8b1.js" rel="prefetch"><link href="/js/chunk-741560ff.31c4b039.js" rel="prefetch"><link href="/js/chunk-9ceacaa2.fe1cc346.js" rel="prefetch"><link href="/js/chunk-c3b00904.1e99b892.js" rel="prefetch"><link href="/css/app.7dc7cdc5.css" rel="preload" as="style"><link href="/css/chunk-vendors.93fa63fb.css" rel="preload" as="style"><link href="/js/app.cf78696a.js" rel="preload" as="script"><link href="/js/chunk-vendors.2503c941.js" rel="preload" as="script"><link href="/css/chunk-vendors.93fa63fb.css" rel="stylesheet"><link href="/css/app.7dc7cdc5.css" rel="stylesheet"></head><body><noscript><strong>We're sorry but vue-app doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div><div id="cc-myssl-id" style="position: fixed;right: 0;bottom: 0;width: 65px;height: 65px;z-index: 99;"><a href="https://myssl.com/www.fanglonger.com?from=mysslid"><img src="https://static.myssl.com/res/images/myssl-id.png" alt="" style="width:100%;height:100%"></a></div><script src="/js/chunk-vendors.2503c941.js"></script><script src="/js/app.cf78696a.js"></script></body></html>
+        })();</script>
+    <link href="/css/chunk-11cc9a46.1127797b.css" rel="prefetch" />
+    <link href="/css/chunk-29cfb7bb.fb2a93c7.css" rel="prefetch" />
+    <link href="/css/chunk-2bc080a1.4af0aaa8.css" rel="prefetch" />
+    <link href="/css/chunk-2d4fc60c.1127797b.css" rel="prefetch" />
+    <link href="/css/chunk-30f50f1a.1127797b.css" rel="prefetch" />
+    <link href="/css/chunk-34761d0b.7a316438.css" rel="prefetch" />
+    <link href="/css/chunk-34e5ce82.dbc488f1.css" rel="prefetch" />
+    <link href="/css/chunk-3b63f688.b6549005.css" rel="prefetch" />
+    <link href="/css/chunk-48313e7b.f0504677.css" rel="prefetch" />
+    <link href="/css/chunk-5bcc2985.1127797b.css" rel="prefetch" />
+    <link href="/css/chunk-62fc455c.1127797b.css" rel="prefetch" />
+    <link href="/css/chunk-631e9f83.dbc488f1.css" rel="prefetch" />
+    <link href="/css/chunk-6cc4fa90.1127797b.css" rel="prefetch" />
+    <link href="/css/chunk-6d4bfa19.630c1498.css" rel="prefetch" />
+    <link href="/css/chunk-741560ff.0c65cad8.css" rel="prefetch" />
+    <link href="/css/chunk-9ceacaa2.c76548f8.css" rel="prefetch" />
+    <link href="/css/chunk-c3b00904.bb5df0d1.css" rel="prefetch" />
+    <script type="text/javascript" src="/js/chunk-08c34318.12fe6bf9.js"></script>
+    <script type="text/javascript" src="/js/chunk-11cc9a46.6598d212.js"></script>
+    <script type="text/javascript" src="/js/chunk-29cfb7bb.5b798f11.js"></script>
+    <script type="text/javascript" src="/js/chunk-2bc080a1.f9084052.js"></script>
+    <script type="text/javascript" src="/js/chunk-2d4fc60c.6b033114.js"></script>
+    <script type="text/javascript" src="/js/chunk-30f50f1a.1099e916.js"></script>
+    <script type="text/javascript" src="/js/chunk-34761d0b.b9256273.js"></script>
+    <script type="text/javascript" src="/js/chunk-34e5ce82.7df214c4.js"></script>
+    <script type="text/javascript" src="/js/chunk-3b63f688.f77fdc31.js"></script>
+    <script type="text/javascript" src="/js/chunk-48313e7b.2f6b569f"></script>
+    <script type="text/javascript" src="/js/chunk-502d2fdd.59c254bd"></script>
+    <script type="text/javascript" src="/js/chunk-5bcc2985.a43d64f1"></script>
+    <script type="text/javascript" src="/js/chunk-62fc455c.9dcb5c0a"></script>
+    <script type="text/javascript" src="/js/chunk-631e9f83.6f677539"></script>
+    <script type="text/javascript" src="/js/chunk-6cc4fa90.925916ca"></script>
+    <script type="text/javascript" src="/js/chunk-6d4bfa19.db75a8b1"></script>
+    <script type="text/javascript" src="/js/chunk-741560ff.31c4b039"></script>
+    <script type="text/javascript" src="/js/chunk-9ceacaa2.fe1cc346"></script>
+    <script type="text/javascript" src="/js/chunk-c3b00904.1e99b892"></script>
+    <link href="/css/app.7dc7cdc5.css" rel="preload" as="style" />
+    <link href="/css/chunk-vendors.93fa63fb.css" rel="preload" as="style" />
+    <script type="text/javascript" src="/js/app.cf78696a.js"></script>
+    <script type="text/javascript" src="/js/chunk-vendors.2503c941.js"></script>
+    <link href="/css/chunk-vendors.93fa63fb.css" rel="stylesheet" />
+    <link href="/css/app.7dc7cdc5.css" rel="stylesheet" />
+</head>
+<body>
+<noscript>
+    <strong>We're sorry but vue-app doesn't work properly without JavaScript enabled. Please enable it to continue.</strong>
+</noscript>
+<div id="app"></div>
+<div id="cc-myssl-id" style="position: fixed;right: 0;bottom: 0;width: 65px;height: 65px;z-index: 99;">
+    <a href="https://myssl.com/www.fanglonger.com?from=mysslid"><img src="https://static.myssl.com/res/images/myssl-id.png" alt="" style="width:100%;height:100%" /></a>
+</div>
+<script type="text/javascript" src="/js/chunk-vendors.2503c941.js"></script>
+<script type="text/javascript" src="/js/app.cf78696a.js"></script>
+</body>
+</html>
