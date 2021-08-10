9725a4ba92680d89eeaa528ca37210a965e1abec
longer <fl140125@gmail.com>
2021-08-10 15:40:31 +0800

    新系统全新升级

--- app/Http/Controllers/Api/v1/ToolsController.php
+++ app/Http/Controllers/Api/v1/ToolsController.php
@@ -35,7 +35,7 @@ class ToolsController extends BaseController
     public function getImageLists()
     {
         validatePost($this->post, ['name' => 'required|string']);
-        $result = $this->imageService->getImageLists($this->post, ['page' => $this->post['page'] || 1, 'limit' => $this->post['limit'] ?? 100]);
+        $result = $this->imageService->getImageLists($this->post, ['page' => $this->post['page'] ?? 1, 'limit' => $this->post['limit'] ?? 100]);
         return ajaxReturn($result);
     }
 }
