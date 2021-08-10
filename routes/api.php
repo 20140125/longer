b039caf280ac9cd0e5a3765cbd1c99eeccbf5235
longer <fl140125@gmail.com>
2021-08-10 15:39:18 +0800

    新系统全新升级

--- routes/api.php
+++ routes/api.php
@@ -105,6 +105,8 @@ Route::middleware('throttle:60,1')->namespace('Api')->prefix('v1')->group(functi
         /* todo:工具管理 */
         Route::match(['get','post'], 'tools/getAddress',  [App\Http\Controllers\Api\v1\ToolsController::class, 'getAddress'])->name('getAddress');
         Route::match(['get','post'], 'tools/getWeather',  [App\Http\Controllers\Api\v1\ToolsController::class, 'getWeather'])->name('getAddress');
+        Route::match(['get','post'], 'tools/getImageLists',  [App\Http\Controllers\Api\v1\ToolsController::class, 'getImageLists'])->name('getImageLists');
+
     });
     /* todo:QQ授权登录 */
     Route::get('oauth/login/qq', [App\Http\Controllers\Api\v1\OauthLoginController::class, 'QQ'])->name('qqLogin');
