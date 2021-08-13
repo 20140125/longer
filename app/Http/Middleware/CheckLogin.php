ab03a543edd3f1bd76be67e2509070554634676b
longer <fl140125@gmail.com>
2021-08-13 14:12:30 +0800

    修改用户授权用户登录标识实时缓存

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -25,6 +25,7 @@ class CheckLogin extends Base
             return $next($request);
         }
         $authorization = $this->userService->getVerifyCode($this->post['token'], $this->post['token']);
+        Log::error(json_encode($authorization));
         if ($authorization['code'] === Code::SUCCESS){
             $_user = $this->userService->getUser(['remember_token' => $this->post['token']]) ?? $this->oauthService->getOauth(['remember_token' => $this->post['token']]);
             $request->merge(array('unauthorized' => $_user));
