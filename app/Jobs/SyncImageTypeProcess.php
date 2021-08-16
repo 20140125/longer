8f8d3252c7f59f81e0daa7a37ed7f79fdf9b30ac
longer <fl140125@gmail.com>
2021-08-16 19:08:40 +0800

    代码优化

--- app/Jobs/SyncImageTypeProcess.php
+++ app/Jobs/SyncImageTypeProcess.php
@@ -37,9 +37,9 @@ class SyncImageTypeProcess implements ShouldQueue
      */
     public function handle()
     {
-        Log::error("longer:sync-spider_image_type $this->post['keywords'] $this->post['uuid']");
+        Log::error("longer:sync-spider_image_type {$this->post['keywords']} {$this->post['uuid']}");
         try {
-            Artisan::call("longer:sync-spider_image_type $this->post['keywords'] $this->post['uuid']");
+            Artisan::call("longer:sync-spider_image_type {$this->post['keywords']} {$this->post['uuid']}");
         } catch (\Exception $exception) {
             WebPush($exception->getMessage(), $this->post['uuid'], 'command');
             Log::error(json_encode(['code' => Code::SERVER_ERROR, 'message' => $exception]));
