49a4dc9865cfb96efbb55926f3df79fdef68470c
longer <fl140125@gmail.com>
2021-08-24 18:23:19 +0800

    新增动态soogif脚本

--- app/Console/Commands/SyncSpiderImageSoogif.php
+++ app/Console/Commands/SyncSpiderImageSoogif.php
@@ -23,6 +23,8 @@ class SyncSpiderImageSoogif extends Command
      */
     protected $description = 'Command description';
 
+    protected $startId;
+
     /**
      * Create a new command instance.
      *
@@ -31,6 +33,7 @@ class SyncSpiderImageSoogif extends Command
     public function __construct()
     {
         parent::__construct();
+        $this->startId = 1;
     }
 
     /**
@@ -48,15 +51,23 @@ class SyncSpiderImageSoogif extends Command
         try {
             $client = new Client();
             $promise = $client->request('GET', $url);
+            sleep(1);
             preg_match("/\d+/", $promise->filter('.float-page a')->first()->html(), $num);
             if ($promise->filter('.float-page a')->first()->html() && (int)$num[0] > 0) {
                 $bar = $this->output->createProgressBar($num[0]);
-                $arr = range(1, $num[0]);
+                if (stristr($url, '_')) {
+                    $this->startId = explode('_', $url)[1];
+                }
+                $arr = range($this->startId, $num[0]);
                 foreach ($arr as $id) {
+                    if (stristr($url, '_')) {
+                        $url = explode('_', $url)[0];
+                    }
                     $href = $url . '_' . $id;
                     $this->info('Current spider link：' . $href);
                     WebPush('Current spider link：' . $href, $this->argument('uuid'), 'command');
                     $promise = $client->request('GET', $href);
+                    sleep(1);
                     $promise->filter('.style-item')->each(function ($node) use ($client) {
                         $arr = ['href' => $node->attr('data-img'), 'name' => $node->filter('.item-tools h2')->text()];
                         if (SooGif::getInstance()->getOne(['href' => $arr['href']])) {
