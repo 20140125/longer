3ee0491c5263482be00a81f9df05f0c64da1a639
longer <fl140125@gmail.com>
2021-11-01 15:47:13 +0800

    代码优化

--- app/Http/Controllers/Service/v1/PushService.php
+++ app/Http/Controllers/Service/v1/PushService.php
@@ -44,6 +44,9 @@ class PushService extends BaseService
         if ($user->role_id != 1) {
             $where[] = ['uuid', $user->uuid];
         }
+        if (!empty($form['username'])) {
+            $where[] = ['username', $form['username']];
+        }
         $this->return['lists'] = $this->pushModel->getLists($where, false, $pagination, $order, $columns);
         foreach ($this->return['lists']['data'] as &$item) {
             $item->created_at = date('Y-m-d H:i:s', $item->created_at);
