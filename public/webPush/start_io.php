8af60992b24fee8c90d7cf7106104788dc2ab62e
longer <fl140125@gmail.com>
2021-11-05 11:14:27 +0800

    代码优化

--- public/webPush/start_io.php
+++ public/webPush/start_io.php
@@ -242,7 +242,7 @@ $sender_io->on('workerStart', function () {
     {
         global $db, $day, $times;
         $oauth = $db->select("FROM_UNIXTIME(created_at,'%Y%m%d') as day,count(*) as total")->from('os_users')
-            ->where("created_at>=" . strtotime(date('Y-m-d 23:59:59', strtotime("-{$times} day"))))
+            ->where("created_at>=" . strtotime(date('Y-m-d 23:59:59', strtotime("-{$times} day"))) . " and state = 'successfully'")
             ->groupBy(["FROM_UNIXTIME(created_at,'%Y%m%d')"])->query();
         $oauthDay = $oauthTotal = array();
         foreach ($oauth as $value) {
