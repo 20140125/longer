a6c494a146c04ec1d9938afc907eb6493fcc5372
longer <fl140125@gmail.com>
2021-12-10 15:46:48 +0800

    前端页面加上百度统计

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'Test'                   => 'App\Console\Commands\Test',
+//        'Test'                   => 'App\Console\Commands\Test',
         'SyncOauth'              => 'App\Console\Commands\SyncOauth',
         'SyncWebPush'            => 'App\Console\Commands\SyncWebPush',
         'SyncCityWeather'        => 'App\Console\Commands\SyncCityWeather',
