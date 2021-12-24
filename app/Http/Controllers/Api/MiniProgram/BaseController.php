768c7f9ce130baa9b9767321a1a89b8f4c48d1d0
longer <fl140125@gmail.com>
2021-12-24 10:08:51 +0800

    代码优化

--- app/Http/Controllers/Api/MiniProgram/BaseController.php
+++ app/Http/Controllers/Api/MiniProgram/BaseController.php
@@ -6,14 +6,22 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Service\MiniProgram\ImageService;
 use App\Http\Controllers\Service\MiniProgram\LoginService;
 use App\Http\Controllers\Service\v1\UserService;
+use App\Models\Api\v1\Users;
 use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
+    /**
+     * @var LoginService $loginService
+     */
     protected $loginService;
-
+    /**
+     * @var ImageService $imageService
+     */
     protected $imageService;
-
+    /**
+     * @var array $post
+     */
     protected $post;
 
     /**
@@ -25,7 +33,8 @@ class BaseController extends Controller
         $this->loginService = LoginService::getInstance();
         $this->imageService = ImageService::getInstance();
         $this->post = $request->post();
-        if (!empty($this->post['token'])) {
+        // 用户标识不为空，并且存在用户信息
+        if (!empty($this->post['token']) && Users::getInstance()->getOne(['remember_token' => $this->post['token']])) {
             UserService::getInstance()->setVerifyCode($this->post['token'], $this->post['token'], config('app.app_refresh_login_time'));
         }
     }
