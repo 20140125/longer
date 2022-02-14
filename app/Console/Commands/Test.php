2d73474f8843e45bdb2d952d319e6b831cd5b869
longer <fl140125@gmail.com>
2022-02-14 11:00:40 +0800

    代码优化

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -1,1 +1,51 @@
 <?php
+
+namespace App\Console\Commands;
+
+use App\Models\Api\v1\Oauth;
+use App\Models\Api\v1\UserCenter;
+use App\Models\Api\v1\Users;
+use Illuminate\Console\Command;
+
+class Test extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'longer:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Testing';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $users = Users::getInstance()->getLists([], [], [], true);
+        foreach ($users as $user) {
+            Oauth::getInstance()->updateOne(['uid' => $user->id], ['remember_token' => $user->remember_token]);
+            $arr = ['u_name' => $user->username, 'token' => $user->remember_token, 'uid' => $user->id, 'notice_status' => 1, 'user_status' => 1];
+            $id = UserCenter::getInstance()->saveOne($arr);
+        }
+    }
+}
+
