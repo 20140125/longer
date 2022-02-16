0894288463b33f54f1796f627b5c660946082ddc
longer <fl140125@gmail.com>
2022-02-16 14:58:19 +0800

    代码优化

--- app/Http/Controllers/Api/v1/UsersController.php
+++ app/Http/Controllers/Api/v1/UsersController.php
@@ -40,7 +40,7 @@ class UsersController extends BaseController
         $rules = [
             'username'     => 'required|string',
             'email'        => 'required|string|email',
-            'phone_number' => 'required|integer|max:11',
+            'phone_number' => 'required|integer|size:11',
             'avatar_url'   => 'required|string|url',
             'id'           => 'required|integer',
             'role_id'      => 'required|integer',
