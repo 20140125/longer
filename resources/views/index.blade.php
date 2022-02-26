4b9e5523f275da6068f6a730bfbc176f89df8b5a
longer <fl140125@gmail.com>
2022-02-26 22:17:52 +0800

    代码优化

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,2 @@
-<!DOCTYPE html>
-<html lang=zh-CN>
-<head>
-    <meta charset=utf-8>
-    <meta name="referrer" content="no-referrer">
-    <meta http-equiv=X-UA-Compatible content="IE=edge">
-    <title>魔盒逗图</title>
-    <script type="text/javascript">
-        (function () {
-            const hm = document.createElement("script");
-            hm.src = "https://hm.baidu.com/hm.js?f89b6a9c4850b466c82a5e1a8e73e76b";
-            const s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(hm, s);
-        })();
-    </script>
-    <script> const coverSupport = 'CSS' in window && typeof CSS.supports === 'function' && (CSS.supports('top: env(a)') || CSS.supports('top: constant(a)'));
-        document.write('<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' + (coverSupport ? ', viewport-fit=cover' : '') + '" />')</script>
-    <link rel=stylesheet href=/h5/static/index.a5c69d49.css>
-</head>
-<body>
-<noscript><strong>Please enable JavaScript to continue.</strong></noscript>
-<div id=app></div>
-<script src=/h5/static/js/chunk-vendors.8f24d31e.js></script>
-<script src=/h5/static/js/index.a9a95ed0.js></script>
-</body>
-</html>
+<!DOCTYPE html><html lang=zh-CN><head><meta charset=utf-8><meta http-equiv=X-UA-Compatible content="IE=edge"><title>魔盒逗图</title><script>var coverSupport = 'CSS' in window && typeof CSS.supports === 'function' && (CSS.supports('top: env(a)') || CSS.supports('top: constant(a)'))
+        document.write('<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' + (coverSupport ? ', viewport-fit=cover' : '') + '" />')</script><link rel=stylesheet href=/h5/static/index.a5c69d49.css></head><body><noscript><strong>Please enable JavaScript to continue.</strong></noscript><div id=app></div><script src=/h5/static/js/chunk-vendors.851630dc.js></script><script src=/h5/static/js/index.82899b54.js></script></body></html>
