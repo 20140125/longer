fdebcdcc0ddf8a02a7b8735f096f3d61381543c8
longer <fl140125@gmail.com>
2022-02-26 21:44:10 +0800

    代码优化

--- app/Http/Controllers/Api/MiniProgram/LoginController.php
+++ app/Http/Controllers/Api/MiniProgram/LoginController.php
@@ -3,28 +3,30 @@
 namespace App\Http\Controllers\Api\MiniProgram;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class LoginController extends BaseController
 {
     /**
      * TODO: 微信获取用户的openid
-     * @param string code 微信code
+     * @param Request $request
      * @return JsonResponse
      */
-    public function getOpenId()
+    public function getOpenId(Request $request)
     {
-        validatePost($this->post, ['code' => 'required|string']);
+        validatePost($request->get('item'), $this->post, ['code' => 'required|string']);
         $result = $this->loginService->getOpenId($this->post);
         return ajaxReturn($result);
     }
 
     /**
      * todo:小程序登录
+     * @param Request $request
      * @return JsonResponse
      */
-    public function login()
+    public function login(Request $request)
     {
-        validatePost($this->post, ['nickName' => 'required|string', 'avatarUrl' => 'required|string', 'code2Session' => 'required|Array']);
+        validatePost($request->get('item'), $this->post, ['nickName' => 'required|string', 'avatarUrl' => 'required|string', 'code2Session' => 'required|Array']);
         $result = $this->loginService->wxLogin($this->post);
         return ajaxReturn($result);
     }
