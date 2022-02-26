c1cdfb23de1c6ccbad73d8264f1d1a5cb5ffc1cc
longer <fl140125@gmail.com>
2022-02-26 21:36:13 +0800

    代码优化

--- app/Http/Controllers/Service/MiniProgram/LoginService.php
+++ app/Http/Controllers/Service/MiniProgram/LoginService.php
@@ -45,8 +45,8 @@ class LoginService extends BaseService
         );
         $curl = new Curl();
         $response = $curl->post($url . http_build_query($data));
+        Log::error(json_encode($response));
         $this->return['lists'] = json_decode(trim($response), true, 512, JSON_OBJECT_AS_ARRAY);
-        Log::error(json_encode($this->return));
         return $this->return;
     }
 
