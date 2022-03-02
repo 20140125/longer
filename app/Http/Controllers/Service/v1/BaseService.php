6affa0d25a33fb2767afee0d0c12b50496653fa6
longer <fl140125@gmail.com>
2022-03-02 10:55:50 +0800

    代码优化

--- app/Http/Controllers/Service/v1/BaseService.php
+++ app/Http/Controllers/Service/v1/BaseService.php
@@ -215,7 +215,8 @@ class BaseService
         }
         \Illuminate\Support\Facades\Log::error(json_encode($_user));
         \Illuminate\Support\Facades\Log::error(json_encode($_role));
-        \Illuminate\Support\Facades\Log::error(json_encode($adCode));
+        \Illuminate\Support\Facades\Log::error($adCode);
+        \Illuminate\Support\Facades\Log::error(json_encode(getCityCode()));
         $area = $this->areaModel->getOne(['code' => $adCode], ['name', 'parent_id', 'info', 'forecast']);
         $province = $this->areaModel->getOne(['id' => $area->parent_id ?? 1], ['name']);
         $this->return['lists'] = array(
