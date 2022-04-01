ea5617f6cfa48993b4246d84138182377e834847
longer <fl140125@gmail.com>
2022-04-01 10:30:48 +0800

    插件管理

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,6 @@
 <!DOCTYPE html>
 <html lang=zh-CN>
 <head>
-    <meta charset=utf-8>
-    <meta name="referrer" content="no-referrer">
-    <meta http-equiv=X-UA-Compatible content="IE=edge">
-    <title>魔盒逗图</title>
     <script>
         var _hmt = _hmt || [];
         (function() {
@@ -14,6 +10,10 @@
             s.parentNode.insertBefore(hm, s);
         })();
     </script>
+    <meta charset=utf-8>
+    <meta name="referrer" content="no-referrer">
+    <meta http-equiv=X-UA-Compatible content="IE=edge">
+    <title>魔盒逗图</title>
     <script>var coverSupport = 'CSS' in window && typeof CSS.supports === 'function' && (CSS.supports('top: env(a)') || CSS.supports('top: constant(a)'))
         document.write('<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' + (coverSupport ? ', viewport-fit=cover' : '') + '" />')</script>
     <link rel=stylesheet href=/h5/static/index.a5c69d49.css>
