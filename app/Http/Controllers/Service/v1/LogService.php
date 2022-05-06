ba6edd945a245060e884cee29a58756ed7ef3963
longer <fl140125@gmail.com>
2022-05-06 11:18:29 +0800

    代码优化

--- app/Http/Controllers/Service/v1/LogService.php
+++ app/Http/Controllers/Service/v1/LogService.php
@@ -61,7 +61,7 @@ class LogService extends BaseService
         $this->return['lists'] = $this->logModel->getLists($where, $pagination, false, ['order' => 'id', 'direction' => 'desc'], ['id', 'username', 'url', 'ip_address', 'created_at', 'day', 'local']);
         foreach ($this->return['lists']['data'] as &$item) {
             $item->created_at = date("Y-m-d H:i:s", $item->created_at);
-        //    $item->log = json_decode($item->log, true);
+            $item->log = [];
         }
         return $this->return;
     }
