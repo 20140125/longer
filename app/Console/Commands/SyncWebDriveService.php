4c477283ed70b095b8930112f40eb35b1c7cf22b
longer <fl140125@gmail.com>
2022-06-30 18:10:54 +0800

    网站爬取

--- app/Console/Commands/SyncWebDriveService.php
+++ app/Console/Commands/SyncWebDriveService.php
@@ -15,7 +15,7 @@ class SyncWebDriveService extends Command
      * @var string
      */
     /* https://www.fabiaoqing.com/tag/detail/id/?.html ? ID变量  */
-    protected $signature = 'longer:sync-web_drive {url=https://www.fabiaoqing.com/biaoqing/lists/page/1.html}';
+    protected $signature = 'longer:sync-web_drive {url=https://www.fabiaoqing.com/biaoqing/lists/page/1.html} {source=1} {tab=false}';
 
     /**
      * The console command description.
@@ -41,9 +41,85 @@ class SyncWebDriveService extends Command
      */
     public function handle(): void
     {
-        putenv('WEBDRIVER_CHROME_DRIVER=D://python/chromedriver.exe');
-        $url = $this->argument('url');
-        $this->getImageLists($url);
+        putenv('WEBDRIVER_CHROME_DRIVER=C://Users/v_llongfang/Desktop/Sign/chromedriver.exe');
+        $this->argument('source') === 1 ? $this->getImageLists($this->argument('url')) : $this->getImageLists2($this->argument('url'));
+    }
+
+    /**
+     * todo:数据抓取
+     * @param $url
+     * @return void
+     */
+    protected function getImageLists2($url)
+    {
+        try {
+            $driver = ChromeDriver::start();
+            $driver->manage()->window()->maximize();
+            $driver->get($url);
+            foreach (range(1, 12, 2) as $k) {
+                sleep(1);
+                $js = "document.documentElement.scrollTop = document.documentElement.scrollHeight * {$k} / 10";
+                $driver->executeScript($js);
+            }
+            $driver->executeScript($js);
+            sleep(3);
+            $this->spiderImage2($driver);
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+        }
+    }
+
+    protected function spiderImage2(ChromeDriver $driver)
+    {
+       try {
+           $this->info($driver->getCurrentURL());
+           // 按钮切换
+           if ($this->argument('tab')) {
+               $btn = $driver->findElements(WebDriverBy::cssSelector('.MuiTabs-flexContainer .MuiTab-wrapper'));
+               foreach ($btn as $b) {
+                   if ($b->getText() === '精选套图') {
+                       $b->click();
+                   }
+               }
+           }
+           $elements = $driver->findElements(WebDriverBy::cssSelector('.jss49 .lazyload-wrapper'));
+           foreach($elements as $item) {
+               if ($this->hasElement($driver,'.jss51')) {
+                   $arr = [
+                       'href' => str_replace('//', 'https://', $item->findElement(WebDriverBy::cssSelector('.jss51'))->getAttribute('src')),
+                       'name' => ['来呀，快活呀', '你怕不是逗比吧', '来啊，造作啊'][rand(0, 2)]
+                   ];
+                   if (SooGif::getInstance()->getOne(['href' => $arr['href']])) {
+                       $this->warn('image already exists: ' . $arr['href']);
+                   } else {
+                       SooGif::getInstance()->saveOne(['href' => $arr['href'], 'name' => $arr['name']]);
+                       $this->info('successfully save image： ' . $arr['href']);
+                   }
+               }
+           }
+       } catch (\Exception $e) {
+           $driver->quit();
+           sleep(5);
+           $this->getImageLists2($this->argument('url'));
+           $this->error($e->getMessage());
+       }
+    }
+
+    /**
+     * todo:判断节点是否存在
+     * @param ChromeDriver $driver
+     * @param string $element
+     * @return bool
+     */
+    protected function hasElement(ChromeDriver $driver, string $element = '#tdialog #secur_code_change img'): bool
+    {
+        try {
+            $driver->findElement(WebDriverBy::cssSelector($element));
+            return true;
+        } catch (\Exception $exception) {
+            $this->info($exception->getMessage());
+            return  false;
+        }
     }
 
     /**
@@ -92,7 +168,11 @@ class SyncWebDriveService extends Command
         $this->currentPageChange($driver);
     }
 
-    /* todo:分页切换 */
+    /**
+     * todo:分页切换
+     * @param ChromeDriver $driver
+     * @return void
+     */
     protected function currentPageChange(ChromeDriver $driver)
     {
         $pageButton = $driver->findElements(WebDriverBy::cssSelector('.pagination a'));
