8b0ebf9556790c61c908ee66e58c1a0e9ff7a4d1
longer <fl140125@gmail.com>
2022-08-19 15:53:28 +0800

    代码优化

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -59,7 +59,7 @@ class CheckLogin extends Base
             $_user = Users::getInstance()->getOne(['remember_token' => $this->post['token']]) ?? Oauth::getInstance()->getOne(['remember_token' => $this->post['token']]);
             /* todo:用户令牌过期 */
             if (empty($_user)) {
-                $request->merge(array('item' => array('code' => Code::UNAUTHORIZED, 'message' => Code::TOKEN_EXPIRED_MESSAGE)));
+                $request->merge(array('item' => array('code' => Code::VALID_TOKEN, 'message' => Code::VALID_TOKEN_MESSAGE)));
                 return $next($request);
             }
             /* todo:角色信息 */
