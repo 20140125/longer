99298377291d52e86b78e25b49b8ec78e110a73e
longer <loveqin0125@foxmail.com>
2022-08-27 10:22:20 +0800

    优化emotion图标查询

--- app/Http/Controllers/Service/v1/EmotionService.php
+++ app/Http/Controllers/Service/v1/EmotionService.php
@@ -37,7 +37,7 @@ class EmotionService extends BaseService
      */
     public function getLists(array $pagination = ['page' => 1, 'limit' => 10], string $where = '', array $order = ['order' => 'id', 'direction' => 'asc'], bool $getAll = false, array $column = ['*']): array
     {
-        $this->return['lists'] = json_decode($this->redisClient->getValue('emotion_type_'.$where));
+        $this->return['lists'] = $this->redisClient->getValue('emotion_type_'.$where);
         if (empty($this->return['lists'])) {
             $this->return['lists'] = $this->emotionModel->getLists($pagination, $where, $order, $getAll, $column);
             if ($getAll) {
