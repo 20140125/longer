08c774be306b090be21cf650dd6b675fa4dbb405
longer <fl140125@gmail.com>
2022-10-18 10:35:16 +0800

    权限优化

---
+++ public/webPush/config/db.example.php
@@ -0,0 +1,8 @@
+<?php
+const HOST = '127.0.0.1';
+const PORT = '3306';
+const USERNAME = 'root';
+const PASSWORD = '123456789';
+const DBNAME = 'root';
+const REDIS_KEY = 'uidConnectionMap';
+const CHAT_KEY = 'uidChatMap'; //在线用户数
